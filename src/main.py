e10df345162a683276b55dee16ca85b44482dfeb
ivar <istangeby@gmail.com>
2020-01-16 15:34:27 +0100

    Addon metadata and register unregister hooks.

---
+++ src/main.py
@@ -0,0 +1,14 @@
+bl_info = {
+    "name": "Shellz",
+    "blender": (2, 80, 0),
+    "category": "object"
+}
+
+
+def register():
+    print("Shellz is now running")
+
+
+def unregister():
+    print("Shellz is now disabled")
+
