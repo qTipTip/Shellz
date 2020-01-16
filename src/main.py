7bfa3f82aae2e5cab44e753d9dbe6c230d045ee0
ivar <istangeby@gmail.com>
2020-01-16 15:47:55 +0100

    Added a test-addon to base things off.

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import bpy
+
 bl_info = {
     "name": "Shellz",
     "blender": (2, 80, 0),
@@ -7,8 +9,26 @@ bl_info = {
 
 def register():
     print("Shellz is now running")
+    bpy.utils.register_class(Shellz)
 
 
 def unregister():
     print("Shellz is now disabled")
+    bpy.utils.unregister_class(Shellz)
 
+
+class Shellz(bpy.types.Operator):
+    """
+    Shellz tooltip
+    """
+
+    def execute(self, **kwargs):
+        """
+        This is run upon calling the operator
+        """
+
+        return {'FINISHED'}
+
+
+if __name__ == '__main__':
+    register()
